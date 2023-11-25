f6725b9399917e76479e9b48e6da8f425416a3d4
unknown <yasserraven@gmail.com>
2023-11-25 18:29:59 +0100

    add the session for every device and store it in the model user

--- main_site/models.py
+++ main_site/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class user(models.Model):
+    sess = models.TextField()
+
+    def __str__(self):
+        return self.sess
--- main_site/views.py
+++ main_site/views.py
@@ -1,7 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import user
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
 def index(request):
-    return render(request, "site_temp/index.html")
+    request.session['init'] = True  # This modifies the session
+    current_user = request.session.session_key  # This gets the session key
+    if current_user is not None:  # This checks if the session key is not None
+        try:
+            user.objects.get(sess=current_user)  # This checks if a user with the current session key already exists
+        except ObjectDoesNotExist:  # This runs if a user with the current session key does not exist
+            new_user = user (
+                sess=current_user
+            )
+            new_user.save()
+    return render(request, "site_temp/index.html", {
+        "user": user , "current_user": current_user  
+    })
